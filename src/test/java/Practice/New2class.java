8cbe76bcc855609196e4accecb53b59c1e84aab4
niranjan388 <niranjanhm44@gmail.com>
2023-02-26 18:02:17 +0530

    First commit

---
+++ src/test/java/Practice/New2class.java
@@ -0,0 +1,22 @@
+package Practice;
+
+public class New2class {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hiii");
+
+
+		//Added in Class 2 Main Branch
+
+
+
+
+
+
+
+
+
+	}
+
+}
---
+++ src/test/java/Practice/NewClass.java
@@ -0,0 +1,11 @@
+package Practice;
+
+public class NewClass {
+
+	public static void main(String[] args) {
+		System.out.println("Hello");
+		System.out.println("Hiii");
+
+	}
+
+}
