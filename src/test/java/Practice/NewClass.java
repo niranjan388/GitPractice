cb80e2515856b0d823ece2e15cfa90b6f30ab7d2
niranjan388 <niranjanhm44@gmail.com>
2023-03-02 22:22:43 +0530

    new

--- src/test/java/Practice/NewClass.java
+++ src/test/java/Practice/NewClass.java
@@ -5,12 +5,8 @@ public class NewClass {
 	public static void main(String[] args) {
 		System.out.println("Hello");
 		System.out.println("Hiii");
-<<<<<<< Updated upstream
-  // new 
-=======
-		// new 
+// stalsh
 
->>>>>>> Stashed changes
 	}
 
 }
