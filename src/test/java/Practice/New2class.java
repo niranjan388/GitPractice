8155d5fcf72b96a09cf937003f53f0cab2ee5c11
niranjan388 <niranjanhm44@gmail.com>
2023-03-06 22:30:04 +0530

    New

--- src/test/java/Practice/New2class.java
+++ src/test/java/Practice/New2class.java
@@ -3,16 +3,14 @@ package Practice;
 public class New2class {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		
+		
 		System.out.println("Hiii");
 
+		//added in local 
 
 		
-		
-
-		//Added in class 2 Niranjan Branch locla
-
-		//Added in class 2 Niranjan Branch Remote Place
 
 
 
