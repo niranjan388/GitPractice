47ec8bc3bb55d5702e2082f70cbf905fcb93c95c
niranjan388 <84678734+niranjan388@users.noreply.github.com>
2023-03-06 22:28:37 +0530

    Update New2class.java

--- src/test/java/Practice/New2class.java
+++ src/test/java/Practice/New2class.java
@@ -3,13 +3,13 @@ package Practice;
 public class New2class {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		
 		System.out.println("Hiii");
-
+//added in remote place
 
 		
 		
-		//Added in class 2 Niranjan Branch
 
 
 
